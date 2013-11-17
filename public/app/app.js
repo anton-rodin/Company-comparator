cdb87a7e239bb3f2926a1cdbe182ac3fe6cbea54
sloot69 <sloot14@gmail.com>
2013-11-17 11:05:44 +0400

    Создал структуру приложения

---
+++ public/app/app.js
@@ -0,0 +1,57 @@
+var MyApp = angular.module('app', [])
+    .run(['$rootScope', 'Stramgraph', function($rootScope, Stramgraph) {
+
+
+        var data = [
+            {
+                "id": 1,
+                "title": "Little Inc",
+                "metrics": {
+                    "offers": 665,
+                    "shares": 20,
+                    "landings": 1124,
+                    "leads": 1102,
+                    "purchases": 88,
+                    "friends": 74
+                }
+            },
+            {
+                "id": 2,
+                "title": "Marvin LLC",
+                "metrics": {
+                    "offers": 20,
+                    "shares": 4,
+                    "landings": 20,
+                    "leads": 25,
+                    "purchases": 28,
+                    "friends": 18
+                }
+            },
+            {
+                "id": 3,
+                "title": "Hodkiewicz, Jacobson and O'Conner",
+                "metrics": {
+                    "offers": 834,
+                    "shares": 8,
+                    "landings": 759,
+                    "leads": 683,
+                    "purchases": 41,
+                    "friends": 35
+                }
+            },
+            {
+                "id": 4,
+                "title": "Harber, Fahey and Berge",
+                "metrics": {
+                    "offers": 233,
+                    "shares": 5,
+                    "landings": 352,
+                    "leads": 348,
+                    "purchases": 31,
+                    "friends": 25
+                }
+            }
+        ];
+
+        Stramgraph.render(document.getElementById('timeline'),data, 200, 500)
+    }]);
---
+++ public/app/services/stramgraph.js
@@ -0,0 +1,112 @@
+(function (ng, app) {
+    "use strict";
+    app.factory('Stramgraph', ["$rootScope", function ($rootScope) {
+
+
+
+        return {
+            render: function(svg, data, width, height) {
+                var metrics = ["offers", "shares", "landings", "leads", "purchases", "friends"],
+                    stack = d3.layout.stack().offset("expand"),
+                    svg = d3.select(svg)
+                        .attr("width", width)
+                        .attr("height", height);
+                var y = d3.scale.linear()
+                    .domain([0, 1])
+                    .range([width, 0]);
+                var color = d3.scale.linear()
+                    .range(["#0D0", "#060"]);
+
+
+                var m = [];
+                metrics.forEach(function (f, metricIndex) {
+                    for (var index in data) {
+                        m[index] = m[index] || [];
+                        m[index].push({x: metricIndex, y:data[index].metrics[f]});
+                    }
+                })
+                var layers = stack(m)
+                layers = data.map(function (f, i) {
+                    return {layer: layers[i], companyName: f.title, color: color(i/(data.length -1))}
+                })
+
+                var x = d3.scale.linear()
+                    .domain([0, metrics.length - 1])
+                    .range([0, height]);
+
+                var area = d3.svg.area()/*.interpolate("basis")*/
+                    .y(function(d) { return x(d.x); })
+                    .x0(function(d) { return y(null); })
+                    .x1(function(d) { return y(d.y0 + d.y); });
+
+                function fixPath (path) {
+                    var Lidx = path.indexOf('L');
+                    var Cidx =  path.slice(Lidx).indexOf('C');
+                    var PCidx = path.slice(0,Lidx).lastIndexOf('C');
+                    var lp = path.substr(PCidx, Lidx-PCidx);
+                    var ss = path.substr(Lidx, Cidx);
+                    return (path.slice(0,Lidx) + lp + ss + path.slice(Lidx));
+                }
+
+                //enter
+                svg.selectAll("path")
+                    .data(layers.reverse())
+                    .enter().append("path")
+                    .attr("d", function (d) {
+                        return fixPath(area(d.layer));
+                    })
+                    .style("fill", function(d) { return d.color; });
+
+                //update
+                d3.selectAll("path")
+                    .data(layers)
+                    .transition()
+                    .duration(2000)
+                    .attr("d", function (d) {return fixPath(area(d.layer));});
+
+
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
+
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    .text("Temperature (ºF)");
+
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .attr("transform", "translate(" + width + ", 0)")
+                    .call(yAxis.orient("right"));
+
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis.orient("left"));
+
+
+            }
+        };
+    }]);
+
+})(angular, MyApp);
