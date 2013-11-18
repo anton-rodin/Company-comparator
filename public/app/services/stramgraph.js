938215af46eb3a32a0b8932a45f989e45fa79c55
sloot69 <sloot14@gmail.com>
2013-11-18 12:18:27 +0400

    выровнял график

--- public/app/services/stramgraph.js
+++ public/app/services/stramgraph.js
@@ -2,68 +2,114 @@
     "use strict";
     app.factory('Stramgraph', ["$rootScope", function ($rootScope) {
 
+        var svgG,
+            metrics = ["offers", "shares", "landings", "leads", "purchases", "friends"];
 
+        function getSvg(svg) {
+            if (!svgG) {
+
+            }
+        }
+
+
+        function formatMetric(d) {
+            return metrics[d];
+        }
 
         return {
-            render: function(svg, data, width, height) {
-                var metrics = ["offers", "shares", "landings", "leads", "purchases", "friends"],
-                    stack = d3.layout.stack().offset("expand"),
-                    svg = d3.select(svg)
+            render: function (svg, data, width, height) {
+
+                var m = [10, 80, 10, 10]; // margins
+                var w = width - m[1] - m[3]; // width
+                var h = height - m[0] - m[2]; // height
+
+                if (!svgG) {
+                    svgG = d3.select(svg)
                         .attr("width", width)
-                        .attr("height", height);
-                var y = d3.scale.linear()
-                    .domain([0, 1])
-                    .range([width, 0]);
-                var color = d3.scale.linear()
-                    .range(["#0D0", "#060"]);
+                        .attr("height", height)
+                        .append("svg:g")
+                        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+                }
+                svg = svgG;
+
+                var stack = d3.layout.stack().offset("expand"),
+                    y = d3.scale.linear()
+                        .domain([0, 1])
+                        .range([w, 0]),
+                    color = d3.scale.linear()
+                        .range(["#15b5c1", "#A5D9DD"]);
 
 
-                var m = [];
+                var formatedData = [];
                 metrics.forEach(function (f, metricIndex) {
                     for (var index in data) {
-                        m[index] = m[index] || [];
-                        m[index].push({x: metricIndex, y:data[index].metrics[f]});
+                        formatedData[index] = formatedData[index] || [];
+                        formatedData[index].push({x: metricIndex, y: data[index].metrics[f]});
                     }
-                })
-                var layers = stack(m)
+                });
+                var layers = stack(formatedData);
                 layers = data.map(function (f, i) {
-                    return {layer: layers[i], companyName: f.title, color: color(i/(data.length -1))}
+                    return {layer: layers[i], companyName: f.title}
                 })
 
                 var x = d3.scale.linear()
                     .domain([0, metrics.length - 1])
-                    .range([0, height]);
+                    .range([0, h]);
 
                 var area = d3.svg.area()/*.interpolate("basis")*/
-                    .y(function(d) { return x(d.x); })
-                    .x0(function(d) { return y(null); })
-                    .x1(function(d) { return y(d.y0 + d.y); });
+                    .y(function (d) {
+                        return x(d.x);
+                    })
+                    .x0(function (d) {
+                        return y(null);
+                    })
+                    .x1(function (d) {
+                        return y(d.y0 + d.y);
+                    });
 
-                function fixPath (path) {
+                function fixPath(path) {
                     var Lidx = path.indexOf('L');
-                    var Cidx =  path.slice(Lidx).indexOf('C');
-                    var PCidx = path.slice(0,Lidx).lastIndexOf('C');
-                    var lp = path.substr(PCidx, Lidx-PCidx);
+                    var Cidx = path.slice(Lidx).indexOf('C');
+                    var PCidx = path.slice(0, Lidx).lastIndexOf('C');
+                    var lp = path.substr(PCidx, Lidx - PCidx);
                     var ss = path.substr(Lidx, Cidx);
-                    return (path.slice(0,Lidx) + lp + ss + path.slice(Lidx));
+                    return (path.slice(0, Lidx) + lp + ss + path.slice(Lidx));
                 }
 
                 //enter
-                svg.selectAll("path")
+                svg.selectAll("path").filter(":not(.domain)")
                     .data(layers.reverse())
                     .enter().append("path")
-                    .attr("d", function (d) {
+                    .attr("d", function (d, i) {
                         return fixPath(area(d.layer));
-                    })
-                    .style("fill", function(d) { return d.color; });
+                    });
 
                 //update
-                d3.selectAll("path")
+                svg.selectAll("path").filter(":not(.domain)")
                     .data(layers)
                     .transition()
                     .duration(2000)
-                    .attr("d", function (d) {return fixPath(area(d.layer));});
+                    .attr("d", function (d, i) {
+                        d3.select(this).style({
+                            "fill": color(i / 8),
+                            "stroke": "#108a93",
+                            "stroke-width": "0.4px"
+                        });
 
+                        return fixPath(area(d.layer));
+                    });
+
+
+                svg.selectAll('.y.axis').remove();
+                // create left yAxis
+                var yAxisLeft = d3.svg.axis().scale(x).ticks(6).tickFormat(formatMetric).tickSize(-w).orient("right").tickSubdivide(true);
+                // Add the y-axis to the left
+                svg.append("svg:g")
+                    .attr("class", "y axis")
+                    .attr("transform", "translate(" + w + ",0)")
+                    .style('fill', '#fff')
+                    .call(yAxisLeft);
 
 
             }
